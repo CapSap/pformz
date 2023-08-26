96f76d07abb01cabd53f08612424359e898b18ea
CapSap <cshotam@gmail.com>
2023-08-27 08:40:14 +1000

    wip

--- app/online-order-refund/page.tsx
+++ app/online-order-refund/page.tsx
@@ -1,10 +1,48 @@
 const OnlineRefundForm = () => {
+  const submitIbts = async (data) => {
+    "use server";
+    const ibts = await data.get("ibt");
+    const author = await data.get("author");
+
+    console.log(ibts);
+
+    if (ibts.match(/[^\D\r\n]+/g)) {
+      console.log("match", ibts);
+      console.log(ibts.match(/[^\D\r\n]+/g));
+
+      const ticketPayload = ibts.match(/[^\D\r\n]+/g).map((ibt) => {
+        return {
+          comment: {
+            body: `There is an issue with this IBT ${ibt}. issue was found by ${author}`,
+          },
+          subject: `IBT with a problem: IBT no ${ibt}`,
+          tags: ["of_todo", "problem_ibt"],
+        };
+      });
+
+      console.log(ticketPayload);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form action={submitIbts} className="flex flex-col justify-center m-12">
         <label htmlFor="ibt">IBT: </label>
-        <input name="ibt" type="text"></input>
-        <button>Submit</button>
+        <textarea className="border border-black" name="ibt"></textarea>
+
+        <label htmlFor="author">Your Name: </label>
+        <input
+          className="border border-black mb-3"
+          required={true}
+          name="author"
+        ></input>
+
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Send problem ibts to zendesk
+        </button>
       </form>
     </div>
   );
