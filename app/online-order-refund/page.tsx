ad1e87a9817e9d94d712cc27855ec467c09638dd
CapSap <cshotam@gmail.com>
2023-08-26 23:48:44 +1000

    add basic form

--- app/online-order-refund/page.tsx
+++ app/online-order-refund/page.tsx
@@ -1,5 +1,13 @@
 const OnlineRefundForm = () => {
-  return <div>form</div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor="ibt">IBT: </label>
+        <input name="ibt" type="text"></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default OnlineRefundForm;
