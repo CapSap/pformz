f68aca4d244674177e95cdb5f493d7c089732e89
CapSap <cshotam@gmail.com>
2023-09-03 22:58:38 +1000

    wip. having trouble with readable stream

--- app/api/problem-ibts/route.ts
+++ app/api/problem-ibts/route.ts
@@ -1,10 +1,52 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: Request, res: Response) => {
-  console.log("post");
+export const POST = async (req: Request) => {
   console.log(req.body);
-  // problem ibt no should be here + user's initals
-  // the above is going to be a JSON string array.
+
+  if (!req.body) {
+    return NextResponse.json({ error: "no body", status: 400 });
+  }
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const base64Encoded = btoa(
+    `${process.env.ZEN_USER}/token:${process.env.ZEN_TOKEN}`,
+  );
+
+  myHeaders.append("Authorization", `Basic ${base64Encoded}`);
+
+  const raw = JSON.stringify({
+    tickets: [
+      {
+        comment: {
+          body: "TEST from next form at " + Date(),
+        },
+        subject: "TEST from next",
+        tags: ["of_todo", "TEST", "problem_ibt"],
+        custom_fields: [{ id: "7565917494287", value: "22222" }],
+      },
+    ],
+  });
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  // const result = await fetch(
+  //   "https://paddypallin.zendesk.com/api/v2/tickets/create_many",
+  //   requestOptions,
+  // )
+  //   .then((response) => response.text())
+  //   .then((result) => console.log(result))
+  //   .catch((error) => console.log("error", error));
+
+  // return NextResponse.json(result);
+
+  return NextResponse.json({ message: "no worky", bod: req.body });
 };
 
 export const GET = async (req: Request) => {
