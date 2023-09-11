e36c75ad1ec208464104f562525429e0dee2c0b6
CapSap <cshotam@gmail.com>
2023-09-12 08:28:18 +1000

    extract form into a seperate client component and feed the existing tickets + problem ibt numbers in as props

--- app/online-order-refund/page.tsx
+++ app/online-order-refund/page.tsx
@@ -1,38 +1,15 @@
-import { FormEvent, useState } from "react";
-
 import { getData, submitIbts } from "../_utils/serverActions";
+import ProblemIbtForm from "../components/ProblemIbtForm";
 
-const OnlineRefundForm = async () => {
+async function OnlineRefundForm() {
   const existingTickets: { ticket_id: number; problem_ibt: string }[] =
     await getData();
 
-  console.log(
-    "problem ibts",
-    existingTickets.map((ticket) => ticket.problem_ibt),
-  );
-
   return (
     <div>
-      <form action={submitIbts} className="flex flex-col justify-center m-12">
-        <label htmlFor="ibt">IBT: </label>
-        <textarea className="border border-black" name="ibt"></textarea>
-
-        <label htmlFor="author">Your Name: </label>
-        <input
-          className="border border-black mb-3"
-          required={true}
-          name="author"
-        ></input>
-
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Send problem ibts to zendesk
-        </button>
-      </form>
+      <ProblemIbtForm existingTickets={existingTickets} />
     </div>
   );
-};
+}
 
 export default OnlineRefundForm;
