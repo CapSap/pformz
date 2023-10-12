10739a86b380967e0fbd11623f8bae2163f3fed1
CapSap <cshotam@gmail.com>
2023-10-12 17:18:24 +1100

    create custom css for the landing page background shape

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,13 @@ export default function Home() {
 
   if (!isSignedIn) {
     return (
-      <div>hello and welcome to the paddy form app. sign in to get started</div>
+      <div>
+        {/* <div className=" h-96 bg-gradient-to-tl from-white to-slate-400"></div> */}
+        <div className="landing-background"></div>
+        <h1 className="text-9xl">pformz</h1>
+        <p>hello and welcome to the paddy form app. sign in to get started</p>
+        <button className="text-2xl bg-slate-200 p-3">Sign in</button>
+      </div>
     );
   } else if (isSignedIn) {
     return (
