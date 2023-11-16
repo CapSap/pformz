41c44d9976f5a10a9245bdedb8ab91f35c65a412
CapSap <cshotam@gmail.com>
2023-11-16 14:15:08 +1100

    add a brief instructions at the top of the page

--- app/submit-bulk-ibts/page.tsx
+++ app/submit-bulk-ibts/page.tsx
@@ -8,7 +8,15 @@ async function OnlineRefundForm() {
   return (
     <div>
       <div className="landing-background opacity-25" />
-
+      <div className="pt-6 ml-6">
+        <h2 className="text-lg">Instructions:</h2>
+        <p className="w-1/3 ">
+          This form will create a ticket for each IBT problem IBT. The ticket
+          will capture the IBT number and your name only (no order numbers). It
+          will also check if the ticket already exists (only if the correct
+          field has been filled in zendesk)
+        </p>
+      </div>
       <ProblemIbtForm existingTickets={existingTickets} />
     </div>
   );
