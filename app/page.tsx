693d9833082a80f4b2b245f4b0812c774129fa61
CapSap <cshotam@gmail.com>
2023-11-20 18:12:44 +1100

    add some hover styles to the sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="ml-60 pt-72 ">
           <h1 className="text-[12rem]">pformz</h1>
           <Link
-            className="text-2xl bg-lime-200 py-3 px-10 ml-32 rounded-2xl border-lime-300 border"
+            className="text-2xl bg-lime-200 py-3 px-10 ml-32 rounded-2xl border-lime-300 border hover:underline hover:bg-lime-300"
             href="/sign-in"
           >
             Sign in
