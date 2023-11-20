c461939b1bac957efc325d672aff33d4a4c3a4ad
CapSap <cshotam@gmail.com>
2023-11-20 23:15:27 +1100

    scaffold form

--- app/stock-checker/page.tsx
+++ app/stock-checker/page.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
 function StockChecker() {
-  return <div>stock checker</div>;
+  const [sku, setSku] = useState<string>();
+
+  console.log(sku);
+
+  return (
+    <div>
+      <p>Input a sku to check which stores have stock</p>
+      <form>
+        <label htmlFor="sku">SKU </label>
+        <input
+          type="text"
+          name="sku"
+          onChange={(e) => {
+            setSku(e.target.value);
+          }}
+        />
+        <button>Get Stock Levels</button>
+      </form>
+    </div>
+  );
 }
 
 export default StockChecker;
