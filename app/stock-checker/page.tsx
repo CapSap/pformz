a62b8e62f41579356a40b1ff1165c91062dab09c
CapSap <cshotam@gmail.com>
2023-11-21 00:21:31 +1100

    add function on client side to handle sending user input to api/check-stock

--- app/stock-checker/page.tsx
+++ app/stock-checker/page.tsx
@@ -1,16 +1,39 @@
 "use client";
 
 import { useState } from "react";
+import { StringLiteral } from "typescript";
+
+type store = {
+  name: string;
+  instore: number;
+  pickup: {};
+  quantity: number;
+  source_code: string;
+  url: string;
+};
 
 function StockChecker() {
   const [sku, setSku] = useState<string>();
 
-  console.log(sku);
+  const [storeStockLevels, setStoreStockLevels] = useState();
+
+  async function checkSku(e) {
+    e.preventDefault();
+    const result = await fetch(`http://localhost:3000/api/stock-checker`, {
+      method: "POST",
+      body: JSON.stringify({ sku: sku }),
+    })
+      .then((res) => res.json())
+      .then((res) => JSON.parse(res.body))
+      .then((res) => setStoreStockLevels(res));
+
+    console.log(result);
+  }
 
   return (
     <div>
       <p>Input a sku to check which stores have stock</p>
-      <form>
+      <form onSubmit={(e) => checkSku(e)}>
         <label htmlFor="sku">SKU </label>
         <input
           type="text"
@@ -21,6 +44,12 @@ function StockChecker() {
         />
         <button>Get Stock Levels</button>
       </form>
+      <div>
+        <h1>Results</h1>
+        {storeStockLevels.map((store) => (
+          <p>store{}</p>
+        ))}
+      </div>
     </div>
   );
 }
