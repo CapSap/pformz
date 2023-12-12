738238615085b7e12e622dc9423b47024c6d6d3e
CapSap <cshotam@gmail.com>
2023-12-12 17:12:54 +1100

    loosely scaffold the fetch request to emailrep. waiting on a free api key

--- app/api/email-checker/route.ts
+++ app/api/email-checker/route.ts
@@ -7,5 +7,16 @@ export const POST = async (req: Request) => {
 
   console.log("route data", emails);
 
+  // this api is limited to 10/day, 250 a month
+  // i am waiting from an api key from emailrep.io
+  // given above limit, i should reserve this check for highly sus emails?
+  //   and do some domain name validation before hand?
+  const firstEmail = emails[0];
+  const emailRepResult = await fetch(`https://emailrep.io/${firstEmail}`).then(
+    (res) => res.json(),
+  );
+
+  console.log(emailRepResult);
+
   return NextResponse.json({ body: "basic response" });
 };
