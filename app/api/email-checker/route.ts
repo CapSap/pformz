aca0cb6dcb83df9a7dd9589dfe2dd53facd5becb
CapSap <cshotam@gmail.com>
2023-12-13 13:46:03 +1100

    add some psudo code for how i think it'll work.
    refactor the request to emailrep so that a request is sent for each email on the array. not doing anything about the limits yet (until i have a key)

--- app/api/email-checker/route.ts
+++ app/api/email-checker/route.ts
@@ -11,12 +11,35 @@ export const POST = async (req: Request) => {
   // i am waiting from an api key from emailrep.io
   // given above limit, i should reserve this check for highly sus emails?
   //   and do some domain name validation before hand?
-  const firstEmail = emails[0];
-  const emailRepResult = await fetch(`https://emailrep.io/${firstEmail}`).then(
-    (res) => res.json(),
+  // const firstEmail = emails[0];
+  // const emailRepResult = await fetch(`https://emailrep.io/${firstEmail}`).then(
+  //   (res) => res.json(),
+  // );
+
+  const emailRepResult = await Promise.all(
+    emails.map(async (email: string) => {
+      return await fetch(`https://emailrep.io/${email}`).then((res) =>
+        res.json(),
+      );
+    }),
   );
-
   console.log(emailRepResult);
+  // for domain name checker; what do i want to do?
 
-  return NextResponse.json({ body: "basic response" });
+  // filter out all the hotmail gmail etc
+  // i think i have to send a request for each email / check.
+  // so something like
+  // tell the user that we are submitting requests 1 by 1.
+  // map over emails array
+  // for each email, send a request and return a promise?
+  // where exactly would the result look like?
+
+  // ideally i think its in an array, each array item is the result of the api call
+  // then let the client read the array and render the results pretty?
+
+  return NextResponse.json({
+    body: {
+      emailRep: emailRepResult,
+    },
+  });
 };
