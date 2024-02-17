839596875a07c86bd2b480731b8efb9cbcca73ae
CapSap <cshotam@gmail.com>
2024-02-17 15:53:34 +1100

    add state for form and handlechanges

--- app/store-request-form/page.tsx
+++ app/store-request-form/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 import React, { useState } from "react";
 
-type items = {
+type item = {
   id: number;
+  quantity: string;
+  sku: string;
+  description: string;
 };
 
 function StoreRequstForm() {
-  const [requestItems, setRequestItems] = useState<items[]>([
-    { id: Date.now() },
+  const [requestItems, setRequestItems] = useState<item[]>([
+    {
+      id: Date.now(),
+      quantity: "",
+      sku: "",
+      description: "",
+    },
   ]);
+
+  const [deliveryMethod, setDeliveryMethod] = useState("store");
+  const [customerName, setCustomerName] = useState<string>();
+  const [customerPhone, setCustomerPhone] = useState<string>();
+  const [customerEmail, setCustomerEmail] = useState<string>();
+  const [customerAddress, setCustomerAddress] = useState<string>();
 
   function addMoreItems(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
@@ -17,19 +31,35 @@ function StoreRequstForm() {
 
   function removeSingleItem(
     e: React.MouseEvent<HTMLButtonElement>,
-    item: items,
+    item: item,
   ) {
     e.preventDefault();
     const newState = requestItems.filter((el, index) => {
-      console.log("filter run count", index);
-      // filter out item that equals the item passed in
-      console.log("el id", el.id);
-      console.log("item", item.id);
       return item.id !== el.id;
     });
     setRequestItems(newState);
   }
 
+  function handleItemChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: number,
+    index: number,
+  ) {
+    const newStateArray = requestItems.map((el) => {
+      if (el.id === id) {
+        return { ...requestItems[index], [e.target.name]: e.target.value };
+      } else {
+        return el;
+      }
+    });
+
+    setRequestItems(newStateArray);
+  }
+
+  function submitForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("subt mit clicked");
+  }
   return (
     <div>
       <h1>store request form</h1>
@@ -39,20 +69,54 @@ function StoreRequstForm() {
         perspiciatis atque, neque vero provident ratione magni? Perferendis iste
         voluptates placeat ex!
       </div>
-      <form action="">
+      <form onSubmit={(e) => submitForm(e)}>
+        <div>
+          <h2>Customer Details</h2>
+          <label htmlFor="customerName">Customer Name</label>
+          <input
+            type="text"
+            name="customerName"
+            onChange={(e) => setCustomerName(e.target.value)}
+            value={customerName}
+          />
+          <label htmlFor="customerPhone">Customer Phone</label>
+          <input
+            type="text"
+            name="customerPhone"
+            onChange={(e) => setCustomerPhone(e.target.value)}
+            value={customerPhone}
+          />
+          <label htmlFor="customerEmail">Customer Email</label>
+          <input
+            type="text"
+            name="customerEmail"
+            onChange={(e) => setCustomerEmail(e.target.value)}
+            value={customerEmail}
+          />
+        </div>
         <div>
           <h2>Item details</h2>
           {requestItems.map((item, index) => (
             <div key={item.id}>
               <label htmlFor="quantity">Quantity</label>
-              <input type="text" />
-
+              <input
+                type="text"
+                name="quantity"
+                value={item.quantity}
+                onChange={(e) => handleItemChange(e, item.id, index)}
+              />
               <label htmlFor="sku">SKU</label>
-              <input type="text" />
-
+              <input
+                type="text"
+                value={item.sku}
+                onChange={(e) => handleItemChange(e, item.id, index)}
+              />
               <label htmlFor="description">Description</label>
-
-              <input type="text" />
+              <input
+                type="text"
+                value={item.sku}
+                onChange={(e) => handleItemChange(e, item.id, index)}
+              />
               <button onClick={(e) => removeSingleItem(e, item)}>
                 Remove item
               </button>
@@ -60,6 +124,38 @@ function StoreRequstForm() {
           ))}
           <button onClick={(e) => addMoreItems(e)}>Add more items</button>
         </div>
+        <div>
+          <h2>Direct to customer or store?</h2>
+          <label htmlFor="deliveryChooser">
+            Where should the product be sent to?
+          </label>
+          <select
+            name="deliveryChooser"
+            id="deliveryChooser"
+            onChange={(e) => {
+              setDeliveryMethod(e.target.value);
+            }}
+          >
+            <option value="store">Store</option>
+            <option value="customerAddress">Customer Address</option>
+          </select>
+          {deliveryMethod === "store" ? (
+            <p>Sending product to your store</p>
+          ) : (
+            <div>
+              <label htmlFor="customerAddress">Customer Address</label>
+              <textarea
+                name="customerAddress"
+                id="customerAddress"
+                cols={30}
+                rows={5}
+                onChange={(e) => setCustomerAddress(e.target.value)}
+                value={customerAddress}
+              ></textarea>
+            </div>
+          )}
+        </div>
+        <button>Send request </button>
       </form>
     </div>
   );
